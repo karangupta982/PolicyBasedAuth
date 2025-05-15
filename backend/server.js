63fdd97429ff262f563b9d948a04d82982051180
Karan gupta <gupta.karan1.gh@gmail.com>
2025-05-15 15:37:22 +0530

    frontend started

--- backend/server.js
+++ backend/server.js
@@ -12,38 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors(
-    // {
-//   origin: 'https://predictcareai.vercel.app'
-//   // origin: 'http://localhost:3000'
-// }
-));
+app.use(cors());
 
 
-
-
-
-
-// app.use('/api/predict', predictionRoutes);
-// app.use("/api/auth", userRoutes);
-// app.use("/api/profile", profileRoutes);
-
-
-
-
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ 
-//     error: 'Internal server error',
-//     status: 'error',
-//     details: {
-//       message: err.message,
-//       type: 'unexpected_error'
-//     }
-//   });
-// });
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
