1fcd9964a8b68a43ff4acae91d12f38a7c97dae3
Karan gupta <gupta.karan1.gh@gmail.com>
2025-05-16 17:50:37 +0530

    frontend Link changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ app.use(cookieParser());
 
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://policy-based-auth.vercel.app', 
+  // origin: 'http://localhost:3000', 
   credentials: true,               
 }));
 
